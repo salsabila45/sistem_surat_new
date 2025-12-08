81a16f90822075a724b3121427eb5b8fc8b8fbd9
irham <i.maulana.azn@gmail.com>
2025-12-08 13:46:29 +0700

    ganti base url ke zeabur

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://layanan-surat-gambarsari.up.railway.app/';
+    public string $baseURL = 'https://layanan-surat-gambarsari.zeabur.app/';
     // public string $baseURL = 'http://localhost:8080/';
 
 
